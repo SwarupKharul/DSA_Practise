967ab6d87f946df495f1dc38ba4783ecc63e6c1a
Swarup Kharul <65753949+SwarupKharul@users.noreply.github.com>
2022-08-01 21:22:47 +0530

    Time: 6 ms (12.58%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
    vector<vector<int> > db;
-    int solve(int m, int n, int i, int j){
-        if(i==0 && j==0)    return 1;
-        if(i<0 || j<0)  return 0;
-        if(db[i][j]) return db[i][j];
-        return db[i][j] = solve(m, n, i-1, j) + solve(m, n, i, j-1);
+    int solve(int m, int n){
+        if(m==0 && n==0)    return 1;
+        if(n<0 || m<0)  return 0;
+        if(db[m][n]) return db[m][n];
+        return db[m][n] = solve(m-1, n) + solve(m, n-1);
     }
     
     int uniquePaths(int m, int n) {
         db.resize(m, vector<int>(n,0));
-        return solve(m, n, m-1, n-1);
+        return solve(m-1, n-1);
     }
 };
