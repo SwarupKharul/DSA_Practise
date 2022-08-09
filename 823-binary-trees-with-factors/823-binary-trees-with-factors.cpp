71bcb3e6082fdfa88e4e7516da92dd106af6515d
Swarup Kharul <65753949+SwarupKharul@users.noreply.github.com>
2022-08-10 01:18:10 +0530

    Time: 104 ms (61.54%), Space: 9.2 MB (81.85%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -4,23 +4,20 @@ public:
         const int M = 1000000007;
         unordered_map<int, long long unsigned int> mp;
         long long unsigned sum = 0;
-        for(auto it: arr)
-            mp[it] = 1;
-        
+
         sort(arr.begin(), arr.end());
         
-        for(auto i=0; i<arr.size(); ++i)
+        for(auto i=0; i<arr.size(); ++i){
+            mp[arr[i]]=1;
             for(auto j=0; j<i; ++j){
                 if(arr[i]%arr[j]==0){
                     if(mp.count(arr[i]/arr[j])>0)
                         mp[arr[i]]+= mp[arr[j]]*mp[arr[i]/arr[j]];
                 }
             }
-        
-        for(auto &it: mp){
-            sum+=it.second;
-            cout<<it.first<<" "<<it.second<<endl;
+            sum+=mp[arr[i]];
         }
+        
         return sum%M;
         
     }
